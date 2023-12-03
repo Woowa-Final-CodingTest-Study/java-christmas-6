7ce44093d4aebe2ab15993d307cec0d6f9d4d8e6
dmswl6310 <dmswl6310@gmail.com>
2023-12-03 15:31:35 +0900

    feat(OutputView,controller): 플래너 소개 문구 표시

---
+++ src/main/java/christmas/controller/EventPlanner.java
@@ -0,0 +1,13 @@
+package christmas.controller;
+
+import christmas.view.OutputView;
+
+public class EventPlanner {
+    public void start() {
+        introducePlanner();
+    }
+
+    public void introducePlanner() {
+        OutputView.printPlannerIntro();
+    }
+}
