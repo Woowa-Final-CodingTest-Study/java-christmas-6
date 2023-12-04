ebb2bb910bd927ad5f7b61f9a8c5307c21bcbd28
endb1228 <85601916+endb1228@users.noreply.github.com>
2023-12-04 10:32:49 +0900

    Feat: controller 및 view 초기화

---
+++ src/main/java/christmas/controller/PromotionController.java
@@ -0,0 +1,23 @@
+package christmas.controller;
+
+import christmas.view.InputView;
+import christmas.view.OutputView;
+
+public class PromotionController {
+    private static PromotionController promotionController;
+
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    private PromotionController(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public static PromotionController getInstance(InputView inputView, OutputView outputView) {
+        if (promotionController == null) {
+            promotionController = new PromotionController(inputView, outputView);
+        }
+        return promotionController;
+    }
+}
